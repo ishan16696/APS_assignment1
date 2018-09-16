6dbd48c7f55363d29862edb3aae3ad7b491c7105
ishan16696 <42602577+ishan16696@users.noreply.github.com>
2018-09-16 23:18:03 +0530

    Update RunningMedian.cpp

--- RunningMedian.cpp
+++ RunningMedian.cpp
@@ -1,23 +1,23 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include<iostream>
+using namespace std;
 #define MAX 1000000
-#define ll long double
-//Median of Stream of Running Integers
+#define ll long long
+//Median of Stream of Running llegers
 
-ll maxHeap[MAX];
-ll minHeap[MAX];
-ll maxHeapSize=0;
-ll minHeapSize=0;
-ll median=0;
+ ll maxHeap[MAX];
+ ll minHeap[MAX];
+ ll maxHeapSize=0;
+ ll minHeapSize=0;
+double median=0;
 
 
-void Min_Heapify(ll position){
-    ll left;
-    ll right;
-    ll temp;
+void Min_Heapify( ll position){
+     ll left;
+     ll right;
+     ll temp;
     left =2*position +1;
     right =2*position +2;
-    ll min=position;
+     ll min=position;
     
     if(minHeap[left] < minHeap[min] && left < minHeapSize)
     {
@@ -43,12 +43,12 @@ void Min_Heapify(ll position){
 }
 
 void Max_Heapify(ll position){
-    ll left;
-    ll temp;
-    ll right;
+     ll left;
+     ll temp;
+     ll right;
     left =2*position +1;
     right =2*position +2;
-    ll max=position;
+     ll max=position;
     
     if(maxHeap[left] > maxHeap[max] && left < maxHeapSize)
     {
@@ -72,7 +72,7 @@ void Max_Heapify(ll position){
     }*/
 }
 
-void Insert_Min(ll y){
+void Insert_Min( ll y){
     minHeap[minHeapSize++]=y;
 
     ll cur = minHeapSize-1;
@@ -88,22 +88,22 @@ void Insert_Min(ll y){
 //    Min_Heapify(0);
 }
 
-ll Extract_Min(){
-    ll x=minHeap[0];
+ ll Extract_Min(){
+     ll x=minHeap[0];
     minHeap[0]=minHeap[minHeapSize-1];
     minHeapSize--;
     Min_Heapify(0);
     return x;
 }
 
-void Insert_Max(ll y){
+void Insert_Max( ll y){
     maxHeap[maxHeapSize++]=y;
     
-    ll cur = maxHeapSize-1;
+     ll cur = maxHeapSize-1;
     
     while( cur!=0 && maxHeap[cur]> maxHeap[(cur-1)/2])
     {
-        ll temp = maxHeap[cur];
+         ll temp = maxHeap[cur];
         maxHeap[cur]=maxHeap[(cur-1)/2];
         maxHeap[(cur-1)/2] = temp;
     
@@ -112,19 +112,19 @@ void Insert_Max(ll y){
     }
     
 //    for(ll i=0;i<maxHeapSize;i++);
-//    prllf("%d\t",maxHeap[i]);
+//    printf("%d\t",maxHeap[i]);
 //    Max_Heapify(0);
 }
 
-ll Extract_Max(){
-    ll x=maxHeap[0];
+ ll Extract_Max(){
+     ll x=maxHeap[0];
     maxHeap[0]=maxHeap[maxHeapSize-1];
     maxHeapSize--;
     Max_Heapify(0);
     return x;
 }
 
-ll insert(ll x){
+double insert(ll x){
      if(maxHeapSize==0 && minHeapSize==0){
          maxHeap[maxHeapSize++]=x;
          median=x;
@@ -132,7 +132,7 @@ ll insert(ll x){
          
      else if(maxHeapSize > minHeapSize){
          if(x < maxHeap[0]){
-             ll y=Extract_Max();
+              ll y=Extract_Max();
              Insert_Min(y);
              Insert_Max(x);
          }
@@ -141,14 +141,14 @@ ll insert(ll x){
              //minHeap[minHeapSize++]=x;
              Insert_Min(x);
          }
-         median=(float)(maxHeap[0]+minHeap[0])/2;
+         median=(double)(maxHeap[0]+minHeap[0])/2;
      }
      
      
      else if(maxHeapSize < minHeapSize)
      {
          if(x > minHeap[0]){
-             ll y=Extract_Min();
+              ll y=Extract_Min();
              Insert_Max(y);
              Insert_Min(x);
          }
@@ -157,11 +157,11 @@ ll insert(ll x){
              //maxHeap[maxHeapSize++]=x;
              Insert_Max(x);
          }
-         median=(float)(maxHeap[0]+minHeap[0])/2;
+         median=(double)(maxHeap[0]+minHeap[0])/2;
      }
      else
      {
-     // when maxHeapSize ==     minHeapSize;
+     // when maxHeapSize == minHeapSize;
       if(x < maxHeap[0]){
           Insert_Max(x);
           median=maxHeap[0];
@@ -177,13 +177,13 @@ ll insert(ll x){
 int main()
     {
     ll test,num;
-    scanf("%lf",&test);
+    scanf("%lld",&test);
     while(test--)
         {
-        scanf("%lf",&num);
-        ll x=insert(num);
+        scanf("%lld",&num);
+        double x=insert(num);
         //float x=findMedian();
-        printf("%lf\n",x);
+        printf("%.1lf\n",x);
         
     }
    return 0; 
